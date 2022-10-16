3773588ccc87c61c3624ed72d1905cd66e959974
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-16 20:17:51 +0530

    Refactor FormControl to make use of dynamic props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -9,12 +9,13 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -58,7 +59,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValidInput && "invalid"}>
+      <FormControl invalid={!isValidInput}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
