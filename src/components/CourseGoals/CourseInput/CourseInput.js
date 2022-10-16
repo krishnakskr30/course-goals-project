6e90a71208d203bf8bc5ba81a7065eb07ed03b9d
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-16 16:14:11 +0530

    Add dynamic inline style to course input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,35 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValidInput, setIsValidInput] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValidInput(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValidInput(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: isValidInput ? "black" : "red" }}>
+          Course Goal
+        </label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{ borderColor: isValidInput ? "#ccc" : "red" }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
