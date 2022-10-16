75c03ebb0aa71cda7a540adb8e71179f6b69f95a
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-16 16:30:22 +0530

    Refactor course input to add dynamic css class to validate input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,15 +25,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: isValidInput ? "black" : "red" }}>
-          Course Goal
-        </label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-          style={{ borderColor: isValidInput ? "#ccc" : "red" }}
-        />
+      <div className={`form-control ${isValidInput ? "" : "invalid"}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
